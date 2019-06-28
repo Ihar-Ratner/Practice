0f1d4ca2ed58680e08ef167861751b77aba0d408
Ihar-Ratner <igo.rat@outlook.com>
2019-06-28 17:26:56 +0300

    third

--- chat-app/src/_reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import messages from './messages';
-import username from './username';
-
-export default combineReducers({
-    messages,
-    username
-})
